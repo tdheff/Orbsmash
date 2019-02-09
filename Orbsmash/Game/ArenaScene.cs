74abb3ebe43f7c5a40db698553a986d68ea94978
Matthew Dean <matt.thomas.dean@gmail.com>
2019-02-09 12:57:15 -0500

    almost didnt include new files

---
+++ Orbsmash/Game/ArenaScene.cs
@@ -0,0 +1,59 @@
+﻿using Handy.Systems;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using Nez;
+using Nez.Tiled;
+using Orbsmash.Player;
+using Scene = Handy.Scene;
+using Handy.Animation;
+using System;
+using System.Collections.Generic;
+using Handy.Components;
+using Orbsmash.Ball;
+using Orbsmash.Constants;
+using Orbsmash.Game.Effects;
+using Orbsmash.Game.Interactions;
+namespace Orbsmash.Game
+{
+    public abstract class ArenaScene : Scene
+    {
+        public Entity CameraEntity = new Entity();
+
+        public ArenaScene() : base(5)
+        {
+            setDesignResolution(1920, 1080, SceneResolutionPolicy.BestFit);
+            LoadContent();
+        }
+
+        protected override void SetupRenderer()
+        {
+            var gameRenderer = new RenderLayerRenderer(-1, new[] { 1, 2, 3 });
+            clearColor = new Color(0.1f, 0.1f, 0.1f);
+            addRenderer(gameRenderer);
+        }
+
+
+        // any scene in the arena format should load all the characters
+        // it's just not that much data so no harm in being simple
+        private void LoadContent()
+        {
+            LoadTextures(new[]
+            {
+                BallSprites.DEFAULT,
+                PlayerSprites.KNIGHT,
+                PlayerSprites.WIZARD,
+                Sprites.HitEffect
+            });
+            LoadAnimationDefinitions(new[]
+            {
+                AsepriteFiles.KNIGHT,
+                AsepriteFiles.WIZARD,
+                AsepriteFiles.HIT_EFFECT
+            });
+
+            var soundsToLoad = new List<string>(KnightSoundEffects.AllEffects);
+            LoadSounds(soundsToLoad.ToArray());
+        }
+    }
+}
---
+++ Orbsmash/Game/PracticeGame.cs
@@ -0,0 +1,111 @@
+﻿using Handy.Systems;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
+using Nez;
+using Nez.Tiled;
+using Orbsmash.Player;
+using System;
+using Handy.Components;
+using Orbsmash.Ball;
+using Orbsmash.Constants;
+using Orbsmash.Game.Effects;
+using Orbsmash.Game.Interactions;
+
+namespace Orbsmash.Game
+{
+    public class PracticeGame : ArenaScene
+    {
+        private Song song;
+        public PracticeGame(): base()
+        {
+            Console.WriteLine("##### Practice Game Start ######");
+            SetupAudio();
+            CreateEntities();
+        }
+
+        private void SetupAudio()
+        {
+            // not sure yet
+        }
+
+        protected override EntitySystem[] Systems()
+        {
+            return new EntitySystem[]
+            {
+                new HitStopSystem(),
+                new TimerSystem(),
+                new CameraShakeSystem(),
+                new ParticleEmitterSystem(),
+                new PlayerInputSystem(),
+                new GameStateMachineSystem(),
+                new BallHitSystem(),
+                new KnockoutSystem(),
+                new KinematicSystem(),
+                new KinematicSystem(),
+                // BALL
+                new BallStateSystem(),
+                // KNIGHT
+                new KnightMovementSystem(),
+                new KnightStateMachineSystem(),
+                new KnightAnimationSystem(),
+                // WIZARD
+                new WizardMovementSystem(),
+                new WizardStateMachineSystem(),
+                new WizardAnimationSystem(),
+                // EFFECTS
+                new HitEffectSystem(),
+                new AnimationSystem()
+            };
+        }
+
+        private void CreateEntities()
+        {
+            var tiledMap = content.Load<TiledMap>(_settings.MapTile);
+            String[] tiledMapLayers = new[]
+            {
+                TiledImportCollisionLayers.BACK_WALLS, TiledImportCollisionLayers.SIDE_WALLS, TiledImportCollisionLayers.NET
+            };
+            int[] tiledMapPhysicsLayers = new[]
+            {
+                PhysicsLayers.BACK_WALLS, PhysicsLayers.SIDE_WALLS, PhysicsLayers.NET
+            };
+            var tiledMapComponent = new Handy.Components.TiledMapComponent(tiledMap, tiledMapLayers, tiledMapPhysicsLayers, true);
+            var entity = new Entity();
+            entity.name = "Map";
+            tiledMapComponent.renderLayer = RenderLayers.BACKGROUND;
+            entity.addComponent(tiledMapComponent);
+            addEntity(entity);
+
+            var gameState = new GameState();
+            gameState.StateEnum = GameStates.Ready;
+            gameState.Players = new Player.Player[_settings.NumPlayers];
+            for (var i = 0; i < _settings.NumPlayers; i++)
+            {
+                var playerSettings = _settings.Players[i];
+                var player = new Player.Player(playerSettings);
+                addEntity(player);
+                gameState.Players[i] = player;
+            }
+
+            var ball = new Ball.Ball();
+            addEntity(ball);
+            gameState.Ball = ball;
+
+            GameStateEntity = new Entity();
+            GameStateEntity.name = "Game";
+            GameStateEntity.addComponent(new GameStateComponent(gameState));
+            GameStateEntity.addComponent(new TimerComponent());
+            GameStateEntity.addComponent(new HitStopComponent());
+            addEntity(GameStateEntity);
+
+            // add camera
+            var cam = new Camera();
+            var cameraShake = new CameraShakeComponent();
+            CameraEntity.addComponent(cam);
+            CameraEntity.addComponent(cameraShake);
+            CameraEntity.name = "Camera";
+            addEntity(CameraEntity);
+        }
+
+    }
+}
