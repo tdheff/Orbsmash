3d591e4a19f8185b3ce24854f3881220c5d690b3
Tommy Heffernan <tdheff@gmail.com>
2019-02-21 12:08:54 -0500

    moved map down

--- Orbsmash/Content/Tiles/Medeival2.tsx
+++ Orbsmash/Content/Tiles/Medeival2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Medeival2" tilewidth="384" tileheight="216" tilecount="1" columns="1">
- <image source="../Sprites/Maps/Medeival.png" width="384" height="216"/>
+<tileset version="1.2" tiledversion="1.2.1" name="Medeival2" tilewidth="448" tileheight="280" tilecount="1" columns="1">
+ <image source="../Sprites/Maps/Medeival.png" width="448" height="280"/>
 </tileset>
