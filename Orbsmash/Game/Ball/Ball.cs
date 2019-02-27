3097c5c6c32ce0eed2669752ef1a894415a6d13c
Tommy Heffernan <tdheff@gmail.com>
2019-02-26 19:15:35 -0500

    playtest

--- Orbsmash/Game/Ball/Ball.cs
+++ Orbsmash/Game/Ball/Ball.cs
@@ -63,9 +63,10 @@ namespace Orbsmash.Ball
              * PARTICLES
              */
             
-            _particleEmitter = new ParticleEmitterComponent(generateParticleEmitterConfig());
+            _particleEmitter = new ParticleEmitterComponent(generateParticleEmitterConfig(gameScene.Textures[BallSprites.DEFAULT]));
             _particleEmitter._active = true;
             _particleEmitter.renderLayer = RenderLayers.PRIMARY;
+            _particleEmitter.renderOffset = -1;
             
             addComponent(_kinematic);
             addComponent(_sprite);
@@ -76,9 +77,13 @@ namespace Orbsmash.Ball
             scale = new Vector2(3, 3);
         }
 
-        private ParticleEmitterConfig generateParticleEmitterConfig()
+        private ParticleEmitterConfig generateParticleEmitterConfig(Texture2D texture)
         {
             var config = scene.content.Load<ParticleEmitterConfig>("Particles/Test_Particles");
+            config.simulateInWorldSpace = true;
+            config.subtexture = Util.ExtractSubtextures(texture, 1, 1)[0];
+            config.blendFuncSource = Blend.One;
+            config.blendFuncDestination = Blend.One;
             config.startColor = Color.Red;
             config.finishColor = Color.Orange;
             config.startColorVariance = Color.Black;
@@ -91,7 +96,10 @@ namespace Orbsmash.Ball
             config.tangentialAccelVariance = 0;
             config.particleLifespan = 0.2f;
             config.particleLifespanVariance = 0.0f;
-            config.startParticleSize = 200.0f;
+            config.startParticleSize = 1000.0f;
+            config.startParticleSizeVariance = 0;
+            config.finishParticleSize = 0.0f;
+            config.sourcePositionVariance = new Vector2(30, 30);
             return config;
         }
     }
